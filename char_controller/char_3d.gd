64cacff19f4ea32737cd0ed0f479f1adf771059b
Staf dierickx <114059414+StafDierickx@users.noreply.github.com>
2025-10-06 16:43:29 +0200

    Fix camera rotation

--- char_controller/char_3d.gd
+++ char_controller/char_3d.gd
@@ -1,6 +1,6 @@
 extends RigidBody3D
 
-@export var look_sensitivity: float = 0.02
+@export var mouse_sensitivity: float = 0.002
 @export var max_walk_speed: float = 5
 @export var walk_acceleration: Curve # Each point on the curve directly represents the acceleration speed for a given normalized velocity.
 @export var air_acceleration: Curve ## Acceleration while airborn
@@ -12,7 +12,7 @@ extends RigidBody3D
 
 @export var MAX_FLOOR_DEG: float = 40
 
-
+@onready var orientation: Node3D = %Orientation
 @onready var camera_gimbal: Node3D = %CameraGimbal
 # labels
 @onready var touching_floor_label: Label = %TouchingFloorLabel
@@ -36,34 +36,6 @@ func _physics_process(delta: float) -> void:
 	coyote_time_left_label.text = str(coyote_time_left)
 	#movement(delta)
 
-func movement_old(delta: float) -> void:
-	var vertical_velocity := Vector3(0, linear_velocity.y, 0)
-	var horizontal_velocity := Vector3(linear_velocity.x, 0, linear_velocity.z)
-	
-	# get player input
-	var input_direction := Input.get_vector("LEFT", "RIGHT", "FORWARD", "BACKWARD").normalized()
-	if not input_direction == Vector2.ZERO:	
-		var desired_direction: Vector3 = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized() # world-space
-		var whish_velocity: Vector3 = desired_direction * max_walk_speed
-	
-		# smooth accel/decel based on curve
-		var accel_factor = walk_acceleration.sample(horizontal_velocity.length() / max_walk_speed)
-		if not is_on_floor:
-			accel_factor = air_acceleration.sample(horizontal_velocity.length() / max_walk_speed)
-		horizontal_velocity = horizontal_velocity.lerp(whish_velocity, accel_factor * delta)
-	elif is_on_floor:
-		pass
-	
-	# recombine with vertical velocity
-	linear_velocity = horizontal_velocity + vertical_velocity
-	
-	# calculate velocity to not exceed maximum by walking/running
-	#if (linear_velocity + delta_velocity).length() > max_walk_speed: # check if added velocity is over speed limit
-		#delta_velocity = linear_velocity.length() /
-		#pass
-
-
-
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	var delta = state.step
 	get_floor_info(state)
@@ -84,7 +56,7 @@ func movement(state: PhysicsDirectBodyState3D, delta) -> void:
 		return
 	
 	# to world-space
-	var desired_direction: Vector3 = transform.basis * Vector3(
+	var desired_direction: Vector3 = orientation.global_transform.basis * Vector3(
 			input_direction.x, 
 			0, 
 			input_direction.y
@@ -133,6 +105,7 @@ func jump(state: PhysicsDirectBodyState3D, delta: float) -> void:
 	if not Input.is_action_just_pressed("ui_accept"):
 		return
 	
+	print("jump")
 	if is_on_floor:
 		state.linear_velocity.y = 5
 	elif coyote_time_left > 0: # coyote time
@@ -153,19 +126,24 @@ func get_floor_info(state: PhysicsDirectBodyState3D) -> void:
 	floor_normal = average_normal.normalized()
 	is_on_floor = true if average_normal != Vector3.ZERO else false
 
-func _unhandled_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	# quit game
 	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_ESCAPE:
 		get_tree().quit()
 
+var yaw = 0.0
+
+func _unhandled_input(event: InputEvent) -> void:
 	# camera movement input logic
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		var mouse_delta = event.relative
-		var delta_x: float = mouse_delta.x * look_sensitivity
-		var delta_y: float = mouse_delta.y * look_sensitivity
+		var delta_x: float = mouse_delta.x * mouse_sensitivity
+		var delta_y: float = mouse_delta.y * mouse_sensitivity
 		
-		# rotate camera and body
-		rotate_y(-delta_x)
-		camera_gimbal.rotate_x(-delta_y)
+		yaw += -delta_x
+		
+		orientation.rotation.y -= delta_x
+		camera_gimbal.rotation.y -= delta_x
+		camera_gimbal.rotation.x -= delta_y
 		camera_gimbal.rotation.x = clamp(camera_gimbal.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 		
