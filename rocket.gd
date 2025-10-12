9d62567fe9824ed744496d3e2810d581de5fae66
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2025-10-12 16:25:38 +0200

    Moved textures

--- rocket.gd
+++ rocket.gd
@@ -1,4 +1,10 @@
 extends Node3D
 
-func _ready() -> void:
-	print("")
+@export var speed = 20
+
+func _process(delta: float) -> void:
+	position += transform.basis * Vector3(0,0, -speed) * delta
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	queue_free()
---
+++ rocket_launcher.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@export var rocket = preload("uid://2tcq4g7p2mhs")
+@export var cooldown_time: float = 1000
+
+@onready var fire_spawn_point: Node3D = $FireSpawnPoint
+
+var fire: bool = false
+@onready var cooldown_timer: Timer = Timer.new()
+
+func _ready() -> void:
+	add_child(cooldown_timer)
+	cooldown_timer.autostart = true
+	cooldown_timer.one_shot = true
+	cooldown_timer.wait_time = cooldown_time / 1000.0
+
+func _physics_process(delta: float) -> void:
+	print(cooldown_timer.time_left)
+	if cooldown_timer.is_stopped() and fire:
+		fire=false
+		cooldown_timer.start()
+		# get rocket ready
+		var instance: Node3D = rocket.instantiate()
+		instance.global_position = fire_spawn_point.global_position
+		instance.transform.basis = fire_spawn_point.global_transform.basis
+		get_tree().root.add_child(instance)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
+		fire = true
